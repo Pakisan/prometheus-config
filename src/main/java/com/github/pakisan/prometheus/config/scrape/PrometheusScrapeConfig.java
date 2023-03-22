81c8ac2a6a4a515c55d6a3f9f960ab227b81ff87
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-22 22:05:09 +0400

    feat: PrometheusScrapeConfig
    - PrometheusScrapeConfig - second part

--- src/main/java/com/github/pakisan/prometheus/config/scrape/PrometheusScrapeConfig.java
+++ src/main/java/com/github/pakisan/prometheus/config/scrape/PrometheusScrapeConfig.java
@@ -164,4 +164,274 @@ public class PrometheusScrapeConfig {
      */
     private Map<String, List<String>> proxy_connect_header;
 
+    /**
+     * List of Azure service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#azure_sd_config">azure_sd_config</a>
+     */
+    private List<Object> azure_sd_configs;
+
+    /**
+     * List of Consul service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#consul_sd_config">consul_sd_config</a>
+     */
+    private List<Object> consul_sd_configs;
+
+    /**
+     * List of DigitalOcean service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#digitalocean_sd_config">digitalocean_sd_config</a>
+     */
+    private List<Object> digitalocean_sd_configs;
+
+    /**
+     * List of Docker service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#docker_sd_config">docker_sd_config</a>
+     */
+    private List<Object> docker_sd_configs;
+
+    /**
+     * List of Docker Swarm service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#dockerswarm_sd_config">dockerswarm_sd_config</a>
+     */
+    private List<Object> dockerswarm_sd_configs;
+
+    /**
+     * List of DNS service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#dns_sd_config">dns_sd_config</a>
+     */
+    private List<Object> dns_sd_configs;
+
+    /**
+     * List of EC2 service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#ec2_sd_config">ec2_sd_config</a>
+     */
+    private List<Object> ec2_sd_configs;
+
+    /**
+     * List of Eureka service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#eureka_sd_config">eureka_sd_config</a>
+     */
+    private List<Object> eureka_sd_configs;
+
+    /**
+     * List of file service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#file_sd_config">file_sd_config</a>
+     */
+    private List<Object> file_sd_configs;
+
+    /**
+     * List of GCE service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#gce_sd_config">gce_sd_config</a>
+     */
+    private List<Object> gce_sd_configs;
+
+    /**
+     * List of Hetzner service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#hetzner_sd_config">hetzner_sd_config</a>
+     */
+    private List<Object> hetzner_sd_configs;
+
+    /**
+     * List of HTTP service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#http_sd_config">http_sd_config</a>
+     */
+    private List<Object> http_sd_configs;
+
+    /**
+     * List of IONOS service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#ionos_sd_config">ionos_sd_config</a>
+     */
+    private List<Object> ionos_sd_configs;
+
+    /**
+     * List of Kubernetes service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#kubernetes_sd_config">kubernetes_sd_config</a>
+     */
+    private List<Object> kubernetes_sd_configs;
+
+    /**
+     * List of Kuma service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#kuma_sd_config">kuma_sd_config</a>
+     */
+    private List<Object> kuma_sd_configs;
+
+    /**
+     * List of Lightsail service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#lightsail_sd_config">lightsail_sd_config</a>
+     */
+    private List<Object> lightsail_sd_configs;
+
+    /**
+     * List of Linode service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#linode_sd_config">linode_sd_config</a>
+     */
+    private List<Object> linode_sd_configs;
+
+    /**
+     * List of Marathon service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#marathon_sd_config">marathon_sd_config</a>
+     */
+    private List<Object> marathon_sd_configs;
+
+    /**
+     * List of AirBnB's Nerve service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#nerve_sd_config">nerve_sd_config</a>
+     */
+    private List<Object> nerve_sd_configs;
+
+    /**
+     * List of Nomad service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#nomad_sd_config">nomad_sd_config</a>
+     */
+    private List<Object> nomad_sd_configs;
+
+    /**
+     * List of OpenStack service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#openstack_sd_config">openstack_sd_config</a>
+     */
+    private List<Object> openstack_sd_configs;
+
+    /**
+     * List of OVHcloud service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#ovhcloud_sd_config">ovhcloud_sd_config</a>
+     */
+    private List<Object> ovhcloud_sd_configs;
+
+    /**
+     * List of PuppetDB service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#puppetdb_sd_config">puppetdb_sd_config</a>
+     */
+    private List<Object> puppetdb_sd_configs;
+
+    /**
+     * List of Scaleway service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#scaleway_sd_config">scaleway_sd_config</a>
+     */
+    private List<Object> scaleway_sd_configs;
+
+    /**
+     * List of Zookeeper Serverset service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#serverset_sd_config">serverset_sd_config</a>
+     */
+    private List<Object> serverset_sd_configs;
+
+    /**
+     * List of Triton service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#triton_sd_config">triton_sd_config</a>
+     */
+    private List<Object> triton_sd_configs;
+
+    /**
+     * List of Uyuni service discovery configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#uyuni_sd_config">uyuni_sd_config</a>
+     */
+    private List<Object> uyuni_sd_configs;
+
+    /**
+     * List of labeled statically configured targets for this job.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#static_config">static_config</a>
+     */
+    private List<Object> static_configs;
+
+    /**
+     * List of target relabel configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#relabel_config">relabel_config</a>
+     */
+    private List<Object> relabel_configs;
+
+    /**
+     * List of metric relabel configurations.
+     * <p>
+     * TODO: implement
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#relabel_config">relabel_config</a>
+     */
+    private List<Object> metric_relabel_configs;
+
 }
