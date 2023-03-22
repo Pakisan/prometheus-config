cd065469b673d51123b1fc8d9ae27c59eea3a3a8
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-22 20:05:07 +0400

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "com.github.pakisan"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    testCompileOnly("org.projectlombok:lombok:1.18.26")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.26")
+
+    implementation("javax.validation:validation-api:2.0.1.Final")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
