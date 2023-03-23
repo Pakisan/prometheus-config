ba8e4557e30acf47bac9817a64a8bd5abdff721d
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2023-03-24 02:42:58 +0400

    feat: DigitaloceanSdConfig

---
+++ src/main/java/com/github/pakisan/prometheus/config/servicediscovery/DigitaloceanSdConfig.java
@@ -0,0 +1,97 @@
+package com.github.pakisan.prometheus.config.servicediscovery;
+
+import com.github.pakisan.prometheus.config.PrometheusTlsConfig;
+import com.github.pakisan.prometheus.config.auth.PrometheusAuthorization;
+import com.github.pakisan.prometheus.config.auth.PrometheusBasicAuth;
+import com.github.pakisan.prometheus.config.auth.PrometheusOAuth2;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Pattern;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Describes Prometheus digitalocean_sd_config.
+ *
+ * @since  1.0.0
+ * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#digitalocean_sd_config">digitalocean_sd_config</a>
+ * @author Pavel Bodiachevskii
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DigitaloceanSdConfig {
+
+    /**
+     * The port to scrape metrics from.
+     */
+    private int port = 80;
+
+    /**
+     * The time after which the droplets are refreshed.
+     */
+    @Pattern(regexp = "((([0-9]+)y)?(([0-9]+)w)?(([0-9]+)d)?(([0-9]+)h)?(([0-9]+)m)?(([0-9]+)s)?(([0-9]+)ms)?|0)")
+    private String refresh_interval = "60s";
+
+    /**
+     * Optional HTTP basic authentication information, currently not support by Azure.
+     */
+    private PrometheusBasicAuth basic_auth;
+
+    /**
+     * Optional `Authorization` header configuration, currently not supported by Azure.
+     */
+    private PrometheusAuthorization authorization;
+
+    /**
+     * Optional OAuth 2.0 configuration, currently not supported by Azure.
+     */
+    private PrometheusOAuth2 oauth2;
+
+    /**
+     * Optional proxy URL.
+     */
+    private String proxy_url;
+
+    /**
+     * Comma-separated string that can contain IPs, CIDR notation, domain names
+     * <p>
+     * that should be excluded from proxying. IP and domain names can
+     * <p>
+     * contain port numbers.
+     */
+    private String no_proxy;
+
+    /**
+     * Use proxy URL indicated by environment variables (HTTP_PROXY, https_proxy, HTTPs_PROXY, https_proxy, and no_proxy)
+     */
+    @Builder.Default
+    private boolean proxy_from_environment = false;
+
+    /**
+     * Specifies headers to send to proxies during CONNECT requests.
+     */
+    private Map<String, List<String>> proxy_connect_header;
+
+    /**
+     * Configure whether HTTP requests follow HTTP 3xx redirects.
+     */
+    @Builder.Default
+    private boolean follow_redirects = true;
+
+    /**
+     * Whether to enable HTTP2.
+     */
+    @Builder.Default
+    private boolean enable_http2 = true;
+
+    /**
+     * TLS configuration.
+     */
+    private PrometheusTlsConfig tls_config;
+
+}
